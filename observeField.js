7a68dbf29a8f2d2f86ea8c4cecd1ac2ee46ac3fb
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-08-17 15:01:23 -0700

    better handling of disposers from StorageContext

--- observeField.js
+++ observeField.js
@@ -1,17 +1,23 @@
 // Copyright (c) 2019 Fall Guy LLC All Rights Reserved.
 
 import * as storage             from './storage';
-import { extendObservable, isObservable, observe } from 'mobx';
+import { extendObservable, isObservable, observe, runInAction } from 'mobx';
 import { deepObserve }          from 'mobx-utils';
+
+//const debugLog = function () {}
+const debugLog = function ( ...args ) { console.log ( '@OBSERVE FIELD:', ...args ); }
 
 //----------------------------------------------------------------//
 export function observeField ( owner, field, callback ) {
 
-    let valueDisposer;
+    let valueDisposer = false;
 
     const setValueObserver = () => {
 
+        debugLog ( 'setting value observer', field );
+
         valueDisposer && valueDisposer (); // not strictly necessary, but why not?
+        valueDisposer = false;
 
         if ( isObservable ( owner [ field ])) {
             valueDisposer = deepObserve ( owner [ field ], callback );
@@ -21,12 +27,18 @@ export function observeField ( owner, field, callback ) {
     setValueObserver ();
 
     let fieldDisposer = observe ( owner, field, ( change ) => {
+
+        debugLog ( 'field did change', field );
+
         setValueObserver ();
         callback ( change );
     });
 
     return () => {
-        fieldDisposer ();
+
+        debugLog ( 'disposing observed field', field );
+
         valueDisposer && valueDisposer ();
+        fieldDisposer ();
     }
 }
