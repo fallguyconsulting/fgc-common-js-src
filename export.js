8e73d1cd63564ceca9c5d3b764b09decd898ef86
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-11-13 17:05:57 -0800

    phone and email field should not be here

--- export.js
+++ export.js
@@ -35,10 +35,8 @@ module.exports = {
 
     ButtonWithDelay:                require ( './ButtonWithDelay.js' ).ButtonWithDelay,
     ClipboardMenuItem:              require ( './ClipboardMenuItem.js' ).ClipboardMenuItem,
-    EmailField:                     require ( './EmailField.js' ).EmailField,
     FilePickerMenuItem:             require ( './FilePickerMenuItem.js' ).FilePickerMenuItem,
     InfiniteScrollView:             require ( './InfiniteScrollView.js' ).InfiniteScrollView,
-    PhoneField:                     require ( './PhoneField.js' ).PhoneField,
     ProgressController:             require ( './ProgressController.js' ).ProgressController,
     ProgressSpinner:                require ( './ProgressSpinner.js' ).ProgressSpinner,
     RevocableContext:               require ( './RevocableContext.js' ).RevocableContext,
