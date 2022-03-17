8befefc01f56ae64655917894b380ff30c4347b4
Sang Bae <sw2.bae@gmail.com>
2022-03-16 17:07:37 -0700

    url editing fix

--- URLField.js
+++ URLField.js
@@ -37,7 +37,7 @@ export const URLField = observer (( props ) => {
         if ( props.value ) {
             update ( props.value );
         }
-    });
+    },[]);
 
     const onChange = ( event ) => {
         setError ( '' );
