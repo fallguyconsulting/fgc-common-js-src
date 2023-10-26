9df8d4f96af7d5a87d57e5df4472a4053e2a829a
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2023-10-26 01:04:55 -0700

    added scanConvert.js

---
+++ draw/scanConvert.js
@@ -0,0 +1,106 @@
+
+//----------------------------------------------------------------//
+export function drawEllipse ( setPixel, x0, y0, x1, y1 ) {
+
+    // https://zingl.github.io/bresenham.html
+
+    [ x0, x1 ]  = sort ( x0, x1 );
+    [ y0, y1 ]  = sort ( y0, y1 );
+
+    x1          = ( x0 < x1 ) ? x1 - 1 : x1;
+    y1          = ( y0 < y1 ) ? y1 - 1 : y1;
+
+    let a       = Math.abs ( x1 - x0 )
+    let b       = Math.abs ( y1 - y0 )
+    let b1      = b & 1;
+
+    let dx      = 4 * ( 1 - a ) * b * b
+    let dy      = 4 * ( b1 + 1 ) * a * a;
+    
+    let err     = dx + dy + ( b1 * a * a );
+    let e2      = 0;
+
+    y0          += Math.floor (( b + 1 ) / 2 );
+    y1          = y0 - b1;
+    a           *= 8 * a;
+    b1          = 8 * b * b;
+
+    do {
+        
+        setPixel ( x1, y0 );
+        setPixel ( x0, y0 );
+        setPixel ( x0, y1 );
+        setPixel ( x1, y1 );
+
+        e2 = 2 * err;
+        
+        if ( e2 <= dy ) {
+            y0++;
+            y1--;
+            err += dy += a;
+        } 
+        
+        if (( e2 >= dx ) || (( 2 * err ) > dy )) {
+            x0++;
+            x1--;
+            err += dx += b1;
+        }
+
+    } while ( x0 <= x1 );
+
+    while ( y0 - y1 < b ) {
+        setPixel ( x0 - 1, y0 );
+        setPixel ( x1 + 1, y0++ ); 
+        setPixel ( x0 - 1, y1 );
+        setPixel ( x1 + 1, y1-- );
+    }
+}
+
+//----------------------------------------------------------------//
+export function drawLine ( setPixel, x0, y0, x1, y1 ) {
+
+    const dx    = Math.abs ( x1 - x0 );
+    const dy    = Math.abs ( y1 - y0 );
+    const sx    = ( x0 < x1 ) ? 1 : -1;
+    const sy    = ( y0 < y1 ) ? 1 : -1;
+
+    let err     = dx - dy;
+    let done    = false;
+
+    while ( !done ) {
+
+        setPixel ( x0, y0 );
+        done = (( x0 === x1 ) && ( y0 === y1 ));
+
+        const e2 = err * 2;
+
+        if ( e2 > -dy ) {
+            err -= dy;
+            x0 += sx;
+        }
+        
+        if ( e2 < dx ) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
+//----------------------------------------------------------------//
+export function fillRect ( setPixel, x0, y0, x1, y1 ) {
+
+    const xMax      = Math.max ( x0, x1 );
+    const yMax      = Math.max ( y0, y1 );
+
+    for ( let y = Math.min ( y0, y1 ); y < yMax; ++y ) {
+        for ( let x = Math.min ( x0, x1 ); x < xMax; ++x ) {
+            setPixel ( x, y );
+        }
+    }
+}
+
+//----------------------------------------------------------------//
+export function sort ( x0, x1 ) {
+
+    return ( x0 < x1 ) ? [ x0, x1 ] : [ x1, x0 ];
+}
