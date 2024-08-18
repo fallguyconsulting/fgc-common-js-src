48ab38351ebdee92a51c59409279e6f2de6cbe92
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2024-08-17 22:19:13 -0700

    +dateutil

---
+++ dateutil.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2019 Fall Guy LLC All Rights Reserved.
+
+import { assert }       from './assert';
+import _                from 'lodash';
+
+//----------------------------------------------------------------//
+export function dateToDisplayString ( date ) {
+
+    if ( !date ) return '';
+
+    // date = new Date ( `${ date.toISOString ().split ( 'T' )[ 0 ]}T00:00:00` );
+    const options = { year: 'numeric', month: 'short', day: '2-digit' };
+    return new Date ( date ).toLocaleDateString ( 'en-GB', options ).replaceAll ( ' ', '-' );
+}
+
+//----------------------------------------------------------------//
+export function dateToHTMLInputString ( date ) {
+
+    if ( !date ) return '';
+    return new Date ( date ).toISOString ().split ( 'T' )[ 0 ];
+} 
+
+//----------------------------------------------------------------//
+export function localDateFromClampedISO ( dateString ) {
+    if ( !dateString ) return null;
+    return new Date ( `${ dateString.split ( 'T' )[ 0 ]}T00:00:00` );
+}
+
+//----------------------------------------------------------------//
+export function localDateToClampedISO ( date ) {
+    if ( !date ) return '';
+    return `${ new Date ( date ).toISOString ().split ( 'T' )[ 0 ]}T00:00:00.000Z`;
+}
